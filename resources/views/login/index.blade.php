577fef5845794202aab1a5ef47b8be5e80482589
Fadhil Gifari Syahputra <144076199+santiana-009@users.noreply.github.com>
2024-12-29 13:04:23 +0700

    Update index.blade.php

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Latihan Laravel</title>
+    <title>Warehouse IT</title>
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/login.css') }}" rel="stylesheet">
     <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
